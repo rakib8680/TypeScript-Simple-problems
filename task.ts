2209c672ed0448e429a544fb881a56928ae5f222
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-08 22:44:36 +0600

    problem no 4 solved

--- task.ts
+++ task.ts
@@ -1,4 +1,5 @@
 
+
 // Problem-1  ......................................
 type Params = string | number;
 const getResult = (param: Params): number => {
@@ -32,4 +33,23 @@ const person1: Person = {
 const getAddressCity = (person: Person): string => {
     return person?.address?.city;
 };
+
+
+
+
+
+
+// Problem-4 ......................................
+type MixedValue = string | number;
+const mixedData: MixedValue[] = [6, 'mango', 2, 8, 'x', 7, 'go', 'think', 3];
+
+const getTotal = (arr: MixedValue[]): number => {
+    let total = 0;
+    arr.forEach(item => {
+        if (typeof item === 'number') {
+            total += item;
+        }
+    })
+    return total;
+}
 
